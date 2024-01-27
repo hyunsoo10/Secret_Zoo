cfe68a1902e0f072423c01a7a19474f50b7fcf42
hyunsoo10 <hyunsoo4885@gmail.com>
2024-01-27 16:48:13 +0900

    feat : 랭킹서버 환경변수 설정

--- ranking/src/main/java/com/example/ranking/player/api/HomeController.java
+++ ranking/src/main/java/com/example/ranking/player/api/HomeController.java
@@ -1,16 +1,21 @@
 package com.example.ranking.player.api;
 
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
 
+    @Value("${server.servlet.context-path}")
+    private String contextPath;
 
     //index.html 기본 페이지
     @GetMapping("/")
-    public String home() {
+    public String home(Model model) {
+        model.addAttribute("contextPath", contextPath);
         return "index.html";
     }
 }
