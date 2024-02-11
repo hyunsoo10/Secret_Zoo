5a1b3d0c73391e2027459acee3cbb11d42efc92e
김재윤 <jaeyun1723@naver.com>
2024-02-11 20:48:36 +0900

    fix.refresh_token 네이밍

--- backend/src/main/java/com/ssafy/fiveguys/game/user/controller/AuthController.java
+++ backend/src/main/java/com/ssafy/fiveguys/game/user/controller/AuthController.java
@@ -50,7 +50,7 @@ public class AuthController {
     @PostMapping("/token/refresh")
     public ResponseEntity<?> reissue(HttpServletRequest request) {
         String accessToken = request.getHeader(HttpHeaders.AUTHORIZATION);
-        String refreshToken = request.getHeader("REFRESHTOKEN");
+        String refreshToken = request.getHeader("refresh_token");
         log.debug("request.getHeader(refresh_token) = {}", refreshToken);
         JwtTokenDto reissuedToken = authService.reissueToken(accessToken, refreshToken);
         return ResponseEntity.status(HttpStatus.OK).body(reissuedToken.responseDto());
