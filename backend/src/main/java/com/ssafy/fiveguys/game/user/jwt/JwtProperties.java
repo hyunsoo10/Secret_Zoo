20ee7f3256440ef8195fcb5b6544cbf879b82ce9
김재윤 <jaeyun1723@naver.com>
2024-02-12 04:24:42 +0900

    fix. accessToken expirationtime 1hour

--- backend/src/main/java/com/ssafy/fiveguys/game/user/jwt/JwtProperties.java
+++ backend/src/main/java/com/ssafy/fiveguys/game/user/jwt/JwtProperties.java
@@ -3,7 +3,7 @@ package com.ssafy.fiveguys.game.user.jwt;
 public interface JwtProperties {
 
     final String SECRET_KEY = "c3ByaW5nYm9vdC1qd3QtdHV0b3JpYWwtc3ByaW5nYm9vdF2qd3QtdHV0b3JpYWntc3ByaW5nYm9vdC1qd3QtdHV0a3JpYWwK";
-    final Long ACCESS_TOKEN_EXPIRATION_TIME = 3000L; // 1시간
+    final Long ACCESS_TOKEN_EXPIRATION_TIME = 1000 * 60 * 60L; // 1시간
     final Long REFRESH_TOKEN_EXPIRATION_TIME = 1209600000L; // 14일
     final String HEADER = "Authorization";
     final String TOKEN_PREFIX = "Bearer ";
