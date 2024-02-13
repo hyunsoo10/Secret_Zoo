c19917ca37aee3d2140adc8d86de2d19ae088358
Gwanu <devkwanwoo@gmail.com>
2024-02-13 15:01:43 +0900

    fix : fix current

--- frontend/client/src/pages/play.jsx
+++ frontend/client/src/pages/play.jsx
@@ -420,7 +420,7 @@ const Play = () => {
           };
         }
         else{
-          prevPlayerListRef.current = playerList;
+          prevPlayerListRef.current = {...playerList};
         }
       }, [playerList]);
       
