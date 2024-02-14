60954d81c113fd393277a5c483ace6e1dc6c5900
김재윤 <jaeyun1723@naver.com>
2024-02-14 10:00:11 +0900

    fix.swagger ui

--- backend/src/main/java/com/ssafy/fiveguys/game/common/config/SwaggerConfig.java
+++ backend/src/main/java/com/ssafy/fiveguys/game/common/config/SwaggerConfig.java
@@ -2,6 +2,7 @@ package com.ssafy.fiveguys.game.common.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
@@ -15,6 +16,8 @@ public class SwaggerConfig {
             .version("1.0.0")
             .title("API 명세서")
             .description("Secret Zoo dev.");
+        Server server=new Server();
+        server.setUrl("https://spring.secretzoo.site");
 
         return new OpenAPI()
             .info(info);
