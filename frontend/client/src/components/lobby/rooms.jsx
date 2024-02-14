a22dd0709e7b5549ffdc0e02bca69f1536535649
card0923 <card0923@naver.com>
2024-02-14 16:47:23 +0900

    chore : 엔터룸 콜백

--- frontend/client/src/components/lobby/rooms.jsx
+++ frontend/client/src/components/lobby/rooms.jsx
@@ -53,7 +53,7 @@ const Rooms = () => {
   // 방입장 
   const enterRoom = (name, password) => {
     socket.emit('enterRoom', name, password, user.userSequence, sessionStorage.getItem('userNickname'), (callback) => {
-      if (callback) {
+      if (callback === 0) {
         dispatch(initRoomName(name));
         sessionStorage.setItem("roomName", name);
         Swal.fire({
@@ -61,13 +61,26 @@ const Rooms = () => {
           "confirmButtonColor": '#3085d6'
         });
         navigate("/play");
-      } else {
+      } else if(callback === 2){
         setOpenEnterModal(false);
         Swal.fire({
           "text": '방이 가득찼습니다. 다른 방을 이용해주세요.',
           "confirmButtonColor": '#3085d6'
         });
+      } else if(callback === 3){
+        setOpenEnterModal(false);
+        Swal.fire({
+          "text": '비밀번호가 틀렸습니다.',
+          "confirmButtonColor": '#3085d6'
+        });
+      } else if(callback === 4){
+        setOpenEnterModal(false);
+        Swal.fire({
+          "text": '이미 게임이 시작됐습니다.',
+          "confirmButtonColor": '#3085d6'
+        });
       }
+
     });
   }
 
@@ -212,7 +225,14 @@ const Rooms = () => {
         <div className="flex flex-wrap  my-2 border-2 overflow-y-auto h-[30em] w-[40em] content-start">
           {Object.keys(rooms).map((key) => (
             <Card href="#" className="w-[47%] h-[30%] m-2"
-              onClick={(e) => { e.preventDefault(); setSelectedRoomName(rooms[key].roomName); setOpenEnterModal(true); }}>
+              onClick={(e) => {
+              if(rooms[key].isLocked){
+                e.preventDefault(); 
+                setSelectedRoomName(rooms[key].roomName); 
+                setOpenEnterModal(true); }
+                else {
+                  enterRoom(rooms[key].roomName,'');
+                }}}>
               {rooms[key].isLocked ? <CiLock></CiLock> : null}
               <p className='truncate text-sm'>{rooms[key].roomName}</p>
               <p>{rooms[key].players[0].playerName}</p>
