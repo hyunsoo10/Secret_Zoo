75c2b51b5eecf078659e7fc964fe3d504f789c9f
card0923 <card0923@naver.com>
2024-02-14 13:00:53 +0900

    feat : 방목록 새로고침

--- frontend/client/src/components/lobby/rooms.jsx
+++ frontend/client/src/components/lobby/rooms.jsx
@@ -106,6 +106,12 @@ const Rooms = () => {
     });
   }
 
+  const noFilter = () => {
+    socket.emit('requestRoomsInfo', (roomsInfo) => {
+      setRooms(roomsInfo);
+    });
+  }
+
   // 검색
   const [searchRoomName, setSearchRoomName] = useState();
   const searchRoom = () => {
@@ -136,6 +142,7 @@ const Rooms = () => {
               <Button color="gray" onClick={filterPlaying}>플레이중</Button>
               <Button color="gray" onClick={filterWait}>대기중</Button>
               <Button color="gray" onClick={filterFull}>꽉찬방</Button>
+              <Button color="gray" onClick={noFilter}>모든방</Button>
             </Button.Group>
           </div>
           <form className="flex justify-end"
